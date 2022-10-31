7d793820c098ed384fdcff33f5942c2d18f57a5d
weitude <eric.c.w.lin@gmail.com>
2022-10-31 15:28:16 +0800

    modify server.js

--- p3/backend/server.js
+++ p3/backend/server.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import cors from 'cors'
-import guessRoute from './routes/course'
+import { createRequire } from "module";
+const require = createRequire(import.meta.url); // construct the require method
+const course_list = require('./course_list.json') // use the require method
+const router = express.Router()
+
+router.get ('/', (_, res) => {
+    res.send("<h1>This is 林佳緯's NTUCourse NEO HW Server</h1>")
+})
+
+router.post('/course', (_, res) => {
+    res.json({courseList: course_list})
+})
+
 const app = express()
 
 app.use(cors())
-// define routes
-app.use('/api/neo', guessRoute)
-// define server
+app.use('/api/neo', router)
 const port = process.env.PORT || 4000
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`)
