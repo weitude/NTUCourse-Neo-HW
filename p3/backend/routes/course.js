3365ac4e0440007c75114ffb2ada0288278d42bb
weitude <eric.c.w.lin@gmail.com>
2022-10-31 15:01:43 +0800

    add root hint

--- p3/backend/routes/course.js
+++ p3/backend/routes/course.js
@@ -2,6 +2,10 @@ import express from 'express'
 import course from '../course_list.json'
 const router = express.Router()
 
+router.get ('/', (_, res) => {
+    res.send("<h1>This is 林佳緯's NTUCourse NEO HW Server</h1>")
+})
+
 router.post('/course', (_, res) => {
     res.json({courseList: course})
 })
